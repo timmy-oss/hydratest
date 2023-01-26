06de3ae1c11bfc771382bfa80e5cbf4d973654a7
timmy-oss <timileyinpelumi.dev@gmail.com>
2023-01-26 18:46:50 +0100

    fixes and optimization

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -2,7 +2,7 @@ function Loader() {
   return (
     <div className="fixed z-[1] text-center  bg-white  cursor-wait flex flex-col justify-center items-center  top-0 bottom-0 right-0 left-0 ">
       <div role="status ">
-        <p className="w-[70px] h-[70px] inline-block border-8  border-gray-300  border-t-[#5522A9] rounded-full animate-spin  text-center "></p>
+        <p className=" w-[40px] h-[40px] md:w-[60px] md:h-[60px] inline-block border-8  border-gray-300  border-t-[#5522A9] rounded-full animate-spin  text-center "></p>
 
         <span className="sr-only">Loading...</span>
       </div>
