7a6246d5188868151c0a6b241fb0da43a7585d78
Timileyin Pelumi <timileyinpelumi.dev@gmail.com>
2023-11-04 22:35:22 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ module.exports = {
   reactStrictMode: true,
 
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "fabaaw.infura-ipfs.io",
-        // port: "",
-        // pathname: "/account123/**",
-      },
-
-       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-       
-      },
-    ],
+    domains: ['fabaaw.infura-ipfs.io', 'res.cloudinary.com'],
   },
 };
